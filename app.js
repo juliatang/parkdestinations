ae704bcd9e0bf11806f1ed3888bac7e709d26a1e
juliatang <37386223+juliatang@users.noreply.github.com>
2021-08-18 14:49:43 -0400

    register form

--- app.js
+++ app.js
@@ -10,10 +10,9 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 
-
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
-const { URLSearchParams } = require('url');
+const userRoutes = require('./routes/users');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -64,15 +63,15 @@ app.use((req, res, next) => {
     next();
 })
 
-app.get('/fakeUser', async (req, res) => {
-    const user = new User({ email: 'juliaaa@gmail.com', username: 'juliaaa' });
-    const newUser = await User.register(user, 'chicken');
-    res.send(newUser);
-});
+// app.get('/fakeUser', async (req, res) => {
+//     const user = new User({ email: 'juliaaa@gmail.com', username: 'juliaaa' });
+//     const newUser = await User.register(user, 'chicken');
+//     res.send(newUser);
+// });
 
-
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/reviews', reviews);
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
 
 app.get('/', (req, res) => {
     res.render('home');
