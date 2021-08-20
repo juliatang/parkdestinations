01639c4e5dc3af1fd6e6822ec157763f8516b0ec
juliatang <37386223+juliatang@users.noreply.github.com>
2021-08-20 13:29:47 -0400

    reviews authorization

--- middleware.js
+++ middleware.js
@@ -2,6 +2,7 @@ const { campgroundSchema, reviewSchema } = require('./schemas.js');
 const ExpressError = require('./utils/ExpressError');
 
 const Campground = require('./models/campground');
+const Review = require('./models/review');
 
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
@@ -27,7 +28,7 @@ module.exports.isAuthor = async (req, res, next) => {
     const campground = await Campground.findById(id);
     if (!campground.author.equals(req.user._id)) {
         req.flash('error', 'You do not have permission to do that!');
-        res.redirect(`/campgrounds/${id}`);
+        return res.redirect(`/campgrounds/${id}`);
     }
     next();
 }
@@ -40,4 +41,14 @@ module.exports.validateReview = (req, res, next) => {
     } else {
         next();
     }
+}
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+    const { id, reviewId } = req.params;
+    const review = await Review.findById(reviewId);
+    if (!review.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have permission to do that!');
+        return res.redirect(`/campgrounds/${id}`);
+    }
+    next();
 }
