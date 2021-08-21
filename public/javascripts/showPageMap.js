697c4b2689539b51f6f16d9b81bd4bd680c0a07b
juliatang <37386223+juliatang@users.noreply.github.com>
2021-08-21 19:10:09 -0400

    customization of map popup

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,11 +1,17 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11',
+    style: 'mapbox://styles/mapbox/light-v10',
     center: JSON.parse(campground).geometry.coordinates,
     zoom: 9
 });
 
 new mapboxgl.Marker()
     .setLngLat(JSON.parse(campground).geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 })
+            .setHTML(
+                `<h3>${JSON.parse(campground).title}</h3>`
+            )
+    )
     .addTo(map);
